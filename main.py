d2ef226b2251000390d18dc0319ea33b6ab9871d
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 17:28:38 +0500

    scraped price from amazon

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.google.com/')
+driver.get('https://www.amazon.com/Learning-Python-Second-Fran%C3%A7ois-Chollet/dp/1617296864/ref=sr_1_1?crid=J1V151KWX9ZL')
 
-driver.close() #close that particular tab
-driver.quit() #close entire browser
+price = driver.find_element(By.CLASS_NAME, 'a-price-whole').text
+fraction = driver.find_element(By.CLASS_NAME, 'a-price-fraction').text
+print(f"The price is {price}.{fraction}")
+# driver.close() #close that particular tab
+# driver.quit() #close entire browser
