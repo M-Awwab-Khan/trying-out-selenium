e4ec5f48676cb20022acaaf2526d75c46afc872d
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 18:39:33 +0500

    interacted with search bar and enter key

--- interaction.py
+++ interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
@@ -7,6 +8,14 @@ chrome_options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://en.wikipedia.org/wiki/Main_Page')
 
-articlecount_parent = driver.find_element(By.ID, 'articlecount')
-article_count = articlecount_parent.find_element(By.TAG_NAME, 'a').text
-print(article_count)
+# scraping article count
+# articlecount_parent = driver.find_element(By.ID, 'articlecount')
+# article_count = articlecount_parent.find_element(By.TAG_NAME, 'a')
+# print(article_count.text)
+
+# article_count.click()
+
+# interacting with wikipedia
+# searching through search bar
+search_bar = driver.find_element(By.NAME, 'search')
+search_bar.send_keys('Python', Keys.ENTER)
