418b16900c247c3229d01c721da1c7bdb2581770
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 16:46:44 +0500

    basic selenium setup working

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.google.com/')
+
+driver.close() #close that particular tab
+driver.quit() #close entire browser
